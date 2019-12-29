76f779afd9bfd298329a171f8355d5c9f14da02f
laurarosaliaticii <59283430+laurarosaliaticii@users.noreply.github.com>
2019-12-29 13:39:09 +0100

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -2,25 +2,51 @@
 
 
 
+
+
+
+
 let myImage = document.querySelector('img');
+
+
+
+
 
 
 
 myImage.onclick = function() {
 
+
+
   let mySrc = myImage.getAttribute('src');
 
-  if(mySrc === 'images/Messi.png') {
 
-    myImage.setAttribute ('src','images/Simon Biles.png');
+
+  if(mySrc === 'images/Simon Biles.png') {
+
+
+
+    myImage.setAttribute ('src','images/Dina Averina.png');
+
+
 
   } else {
 
-    myImage.setAttribute ('src','images/Yana Kudrjavceva.png');
+
+
+    myImage.setAttribute ('src','images/Simon Biles.png');
+
+
 
   }
 
+
+
 }
+
+
+
+
 
 
 
@@ -28,61 +54,122 @@ myImage.onclick = function() {
 
 
 
+
+
+
+
 let myButton = document.querySelector('button');
+
+
 
 let myHeading = document.querySelector('h1');
 
 
 
+
+
+
+
 function setUserName() {
 
-  let myName = prompt('¿Cúal es tu deporte favorito?');
+
+
+  let myName = prompt('Please enter your name.');
+
+
 
   if(!myName || myName === null) {
 
+
+
     setUserName();
+
+
 
   } else {
 
+
+
     localStorage.setItem('name', myName);
 
-    myHeading.innerHTML = 'Yana Kudrjavceva es la gimnasta más joven con tantos logros.' + myName;
+
+
+    myHeading.innerHTML = 'Las mejores gimnastas, descúbrelas ' + myName;
+
+
 
   }
 
+
+
 }
+
+
+
+
 
 
 
 if(!localStorage.getItem('name')) {
 
+
+
   setUserName();
+
+
 
 } else {
 
+
+
   let storedName = localStorage.getItem('name');
 
-  myHeading.innerHTML = 'Yana Kudrjavceva es la gimnasta más joven con tantos logros.' + storedName;
+
+
+  myHeading.innerHTML = 'Las mejores gimnastas, descúbrelas ' + storedName;
+
+
 
 }
+
+
+
+
 
 
 
 myButton.onclick = function() {
 
+
+
   setUserName();
+
+
 
 }
 
 
+
+
+
    var catEl = document.getElementById("cat");
+
  
+
   var startTime = new Date().getTime();
+
   var walkTheCat = function() {
+
       var currTime = new Date().getTime();
+
       var secondsElapsed = ((currTime - startTime)/1000);
+
    var newLeft= secondsElapsed*77 + "px";
+
     catEl.style.left= newLeft;
+
     window.requestAnimationFrame(walkTheCat);
+
   };
+
   walkTheCat();
